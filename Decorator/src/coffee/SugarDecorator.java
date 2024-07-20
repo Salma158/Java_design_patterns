713081345ce7210492aed5364dab6cc9d5a8d76d
Salma158 <salmasherif856@gmail.com>
2024-07-21 01:50:45 +0300

    added sugar concrete decorator

---
+++ Decorator/src/coffee/SugarDecorator.java
@@ -0,0 +1,17 @@
+package coffee;
+
+public class SugarDecorator extends CoffeeDecorator {
+    public SugarDecorator(Coffee decoratedCoffee) {
+        super(decoratedCoffee);
+    }
+
+    @Override
+    public String getDescription() {
+        return decoratedCoffee.getDescription() + ",with Sugar";
+    }
+
+    @Override
+    public double getCost() {
+        return decoratedCoffee.getCost() + 10.0;
+    }
+}
