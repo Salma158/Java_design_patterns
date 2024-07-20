ba6f77011e322b6d021faabe0fcdf4ed32e45133
Salma158 <salmasherif856@gmail.com>
2024-07-21 01:41:19 +0300

    added the decorator

---
+++ Decorator/src/coffee/CoffeeDecorator.java
@@ -0,0 +1,20 @@
+package coffee;
+
+public abstract class CoffeeDecorator implements Coffee{
+
+    protected Coffee decoratedCoffee;
+
+    public CoffeeDecorator(Coffee decoratedCoffee) {
+        this.decoratedCoffee = decoratedCoffee;
+    }
+
+    @Override
+    public String getDescription() {
+        return decoratedCoffee.getDescription();
+    }
+
+    @Override
+    public double getCost() {
+        return decoratedCoffee.getCost();
+    }
+}
